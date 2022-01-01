083d30bd85027cc1d98688b8ecdb1d6c3138e517
Viruuus30 <71880004+Viruuus30@users.noreply.github.com>
2022-01-01 14:42:52 +0100

    main

---
+++ workflow/la-maison-jungle/src/components/ShoppingList.js
@@ -0,0 +1,31 @@
+import { plantList } from '../datas/plantList'
+
+function ShoppingList() {
+	const categories = plantList.reduce(
+		(acc, plant) =>
+			acc.includes(plant.category) ? acc : acc.concat(plant.category),
+		[]
+	)
+
+	return (
+		<div>
+			<ul>
+				{categories.map((cat) => (
+					<li key={cat}>{cat}</li>
+				))}
+			</ul>
+			<ul>
+				{plantList.map((plant) => (
+					<li key={plant.id}>{plant.name}</li>
+				))}
+			</ul>
+		</div>
+	)
+}
+
+{plantList.map((plant) => (
+        <li key={ plant.id }>
+            {plant.isBestSale ? <span>🔥</span> : <span>👎</span>}
+        </li>
+    ))}
+export default ShoppingList
